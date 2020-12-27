7847386be0f2ad1153a63b6baf60bf1c2448d371
LGG <lin.guanguo2000@gmail.com>
2020-12-27 19:18:35 +0800

    20201227

---
+++ LGGnet/EventLoop.h
@@ -0,0 +1,18 @@
+#ifndef LGGNET_EVENTLOOP
+#define LGGNET_EVENTLOOP
+
+#include "Noncopyable.h"
+
+namespace LGG
+{
+
+class EventLoop : Noncopyable {
+  private:
+
+  public:
+  
+};
+
+}//end LGG
+
+#endif
