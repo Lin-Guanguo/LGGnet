5f2e797c4ebda73d4ca0d9760727ec29362a1f80
LGG <lin.guanguo2000@gmail.com>
2020-12-27 19:33:16 +0800

    20201227

--- LGGnet/EventLoop.h
+++ LGGnet/EventLoop.h
@@ -2,15 +2,24 @@
 #define LGGNET_EVENTLOOP
 
 #include "Noncopyable.h"
+#include <pthread.h>
 
 namespace LGG
 {
 
 class EventLoop : Noncopyable {
-  private:
-
+    const pid_t threadId_;
   public:
-  
+    EventLoop();
+
+    ~EventLoop();
+
+    void loop();
+
+    bool isInLoopThread() const { return threadId_ == 0; } //
+
+  private:
+    
 };
 
 }//end LGG
--- LGGnet/Noncopyable.h
+++ LGGnet/Noncopyable.h
@@ -13,5 +13,4 @@ class Noncopyable {
 
 }//end LGG
 
-
 #endif
