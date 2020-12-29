21306d516bdfe5ccfd9ae72ede2da8845f3d0328
LGG <lin.guanguo2000@gmail.com>
2020-12-29 23:00:01 +0800

    log

--- LGGnet/Log.h
+++ LGGnet/Log.h
@@ -24,8 +24,8 @@ class LogSetting : StaticClass {
 
     static void setG_LOG_LEVEL(LogLevel newLevel) { G_LOG_LEVEL = newLevel; }
     static LogLevel getG_LOG_LEVEL() { return G_LOG_LEVEL; }
-    static void setG_LOG_FUNCTION(LogFunction fun) { G_LOG_FUNCTION = fun; }
     static LogFunction getG_LOG_FUNCTION() { return G_LOG_FUNCTION; }
+    static void setG_LOG_FUNCTION(LogFunction fun) { G_LOG_FUNCTION = fun; }
 
   private:
     static LogLevel G_LOG_LEVEL;
@@ -35,7 +35,7 @@ class LogSetting : StaticClass {
 
 #define LGG_LOG_DEFINE_TEMPLATE(LOGLEVEL, LEVEL_NAME, ...) { \
     if(LGG::LogSetting::getG_LOG_LEVEL() <= LGG::LogSetting::LOGLEVEL) \
-        LGG::LogSetting::getG_LOG_FUNCTION()(LGG::Format::concatToString(LEVEL_NAME, " file=", __FILE__, ", line=", __LINE__ , ": ", __VA_ARGS__, "\n"));}
+        LGG::LogSetting::getG_LOG_FUNCTION()(LGG::Format::concatToString(LEVEL_NAME, " file=", __FILE__, ", line=", __LINE__ , ": ", ##__VA_ARGS__, "\n"));}
 
 #define LOG_TRACE(...) LGG_LOG_DEFINE_TEMPLATE(TRACE, "TRACE", __VA_ARGS__)
 #define LOG_DEBUG(...) LGG_LOG_DEFINE_TEMPLATE(DEBUG, "DEBUG", __VA_ARGS__)
