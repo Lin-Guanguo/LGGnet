861c0201d2e0c922c0e89bf5d457a3f2254aafcb
LGG <lin.guanguo2000@gmail.com>
2020-12-29 21:20:50 +0800

    format

--- LGGnet/Format.h
+++ LGGnet/Format.h
@@ -19,7 +19,9 @@ class Format : StaticClass{
     static void formatData(int i, std::string& str) { str.append(std::to_string(i)); }
     static void formatData(long i, std::string& str) { str.append(std::to_string(i)); }
     static void formatData(long long i, std::string& str) { str.append(std::to_string(i)); }
+    static void formatData(unsigned int i, std::string& str) { str.append(std::to_string(i)); }
     static void formatData(unsigned long i, std::string& str) { str.append(std::to_string(i)); }
+    static void formatData(unsigned long long i, std::string& str) { str.append(std::to_string(i)); }
     static void formatData(double i, std::string& str) { str.append(std::to_string(i)); }
     static void formatData(const char* cstr, std::string& str) { str.append(cstr); }
     static void formatData(void* ptr, std::string& str) { str.append(std::to_string((long)ptr)); }
