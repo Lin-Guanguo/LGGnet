9d59aaadc19f80db520edcc2172fe9a970f8d939
LGG <lin.guanguo2000@gmail.com>
2020-12-30 19:41:44 +0800

    buffer writefd

--- LGGnet/tool/Buffer.cc
+++ LGGnet/tool/Buffer.cc
@@ -9,7 +9,7 @@ ssize_t Buffer::writefd(int fd) {
     auto readSize = ::read(fd, stackbuf, 1 << 16);
     LOG_TRACE("Buffer ", this, " read ", readSize, " from fd ", fd);
     if(readSize > 0){
-        std::copy(stackbuf, stackbuf + readSize, std::back_insert_iterator(buf_));
+        std::move(stackbuf, stackbuf + readSize, std::back_insert_iterator(buf_));
     }
     return readSize;
 }
