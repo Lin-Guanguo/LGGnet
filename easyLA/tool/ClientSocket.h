6b9e0a08627d45d863568175e51fd7c7658c6459
linguanguo <lin.guanguo2000@gmail.com>
2021-01-13 19:19:29 +0800

    server and client socket

---
+++ easyLA/tool/ClientSocket.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "SocketAPI.h"
+
+namespace LGG
+{
+
+class ClientSocket {
+    int fd_;
+public:
+    ClientSocket(std::string_view addrStr, int port) {
+        LOG_INFO("open clientSocket, server address = ", addrStr, ", port = ", port);
+        fd_ = SocketAPI::newSocket(AF_INET, SOCK_STREAM);
+        SocketAddr addr(AF_INET, addrStr, port);
+        SocketAPI::connect(fd_, addr);
+    };
+
+    ~ClientSocket() {};
+
+    int getFd() { return fd_; }
+};
+
+} // namespace LGG
---
+++ easyLA/tool/ServerSocket.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "SocketAPI.h"
+
+namespace LGG
+{
+
+class ServerSocket{
+    int fd_;
+public:
+    ServerSocket(int port) {
+        LOG_INFO("open serverSocket, port = ", port);
+        fd_ = SocketAPI::newSocket(AF_INET, SOCK_STREAM);
+        SocketAddr addr(AF_INET, "127.0.0.1", port);
+        SocketAPI::bind(fd_, addr);
+        SocketAPI::listen(fd_);
+    };
+
+    ~ServerSocket() { SocketAPI::close(fd_); };
+
+    auto accept() { return SocketAPI::accept(fd_); }
+
+    int getFd() { return fd_; }
+};
+
+
+
+} // namespace LGG
+
