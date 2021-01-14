e650fbdd8326ff6ad03dd056dbcc25bc49c787fb
linguanguo <lin.guanguo2000@gmail.com>
2021-01-14 09:42:15 +0800

    依赖关系调整，.h移动到.cc

--- easyLA/tool/ConnectionSocket.cc
+++ easyLA/tool/ConnectionSocket.cc
@@ -1,42 +1,8 @@
 #include "ConnectionSocket.h"
-#include "SocketAPI.h"
 #include <unistd.h>
 
 using namespace LGG;
 
-ConnectionSocket::~ConnectionSocket() { SocketAPI::close(fd_); };
-
-ssize_t ConnectionSocket::readFromSocket() { return buf_.writeFromFd(fd_); }
-
-std::string ConnectionSocket::readAllBuf() { return buf_.readAll(); }
-
-std::string ConnectionSocket::read(ssize_t len) { 
-    while(buf_.readableSize() < len){
-        readFromSocket();
-    }
-    return buf_.read(len);
+void ConnectionSocket::write(std::string_view str){
+    ::write(fd_, str.data(), str.size());
 }
-
-std::string ConnectionSocket::readLine(){
-    std::string line = buf_.readLine();
-    while(line.size() == 0){
-        if(readFromSocket() == 0){
-            return {};
-        } 
-        line = buf_.readLine();
-    }
-    return line;
-}
-
-std::string ConnectionSocket::seek(ssize_t len) {
-    while(buf_.readableSize() < len){
-        readFromSocket();
-    }
-    return buf_.seek(len);
-}
-
-auto ConnectionSocket::bufSize(){
-    return buf_.readableSize();
-}
-
-void ConnectionSocket::write(std::string_view str);
