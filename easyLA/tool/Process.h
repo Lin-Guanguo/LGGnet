2cf6f19c27619f908c06b400e9d2a046312bcdbb
linguanguo <lin.guanguo2000@gmail.com>
2021-01-14 21:48:17 +0800

    processAPI

--- easyLA/tool/Process.h
+++ easyLA/tool/Process.h
@@ -3,26 +3,61 @@
 #include "StaticClass.h"
 #include "Log.h"
 #include <functional>
+#include <error.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 namespace LGG
 {
 
 class Process{
+    static void sigchldHandler(int sig){
+        auto savedErrno = errno;
+        pid_t childPid;
+        while((childPid = ::waitpid(-1, NULL, WNOHANG)) > 0) {
+            LOG_INFO("sigchldHandler reaped child ", childPid);
+        }
+        if(childPid == -1 && errno != ECHILD){
+            LOG_ERROR("waitpid error");
+        }
+        errno = savedErrno;
+    }
+
 public:
     using ForkTask = std::function<void(void)>;
+
+    static void iniSigchldHandler() {
+        struct sigaction sa;
+        sa.sa_handler = Process::sigchldHandler;
+        sigemptyset(&sa.sa_mask);
+        sa.sa_flags = 0;
+        if(sigaction(SIGCHLD, &sa, NULL) == -1){
+            LOG_FATAL("sigaction error")
+        }
+    }
     
+    //return subprocess's pid
     static pid_t ForkAndRun(const ForkTask& task) {
         pid_t pid = ::fork();
-        if( pid == 0 ){    //subprocess
+        switch(pid){
+        case 0:
             task();
             exit(0);
-        }else if(pid < 0) {
-            LOG_ERROR("fork error, return pid = ", pid);
-        }else {
-            LOG_TRACE("fork succeed, subprocess pid = ", pid);
+            break;
+        case -1:
+            LOG_ERROR("fork error, return pid = ", pid, " main pid = ", ::getpid());
+            break;
+        default:
+            LOG_TRACE("fork succeed, subprocess pid = ", pid, " main pid = ", ::getpid());
         }
         return pid; //父进程获得子进程id，子进程获得0
+    }
+
+    //return subprocess's pid
+    static pid_t ForkAndExecve(const char* pathname, char* const argv[], char* const envp[]) {
+        return ForkAndRun([pathname, argv, envp](){
+            ::execve(pathname, argv, envp);
+        });
     }
 
 };
--- example/process/main.cc
+++ example/process/main.cc
@@ -1,20 +1,13 @@
-#include "../../easyLA/tool/ServerSocket.h"
 #include "../../easyLA/tool/Log.h"
+#include "../../easyLA/tool/Process.h"
 #include <unistd.h>
 
 using namespace std;
 using namespace LGG;
 
 int main(int argc, char** argv){
-    ServerSocket server(8011);
-    int fd = server.getFd();
-
-    char hello[] = "hello";
-    int len = sizeof(hello);
-    for(;;){
-        auto connection = server.accept();
-        LOG_INFO(connection.getAddr().toStringAsIPV4());
-        auto line = connection.readLine();
-        connection.write(line);
-    }
+    Process::ForkAndRun([](){
+        Process::ForkAndExecve("./helloworld", NULL, NULL);
+    });
+    LOG_INFO("main end pid = ", ::getpid());
 }
