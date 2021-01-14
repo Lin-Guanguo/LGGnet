f0ad19ecfba6d11583bc30ad347a11f0455c3b79
linguanguo <lin.guanguo2000@gmail.com>
2021-01-14 13:39:24 +0800

    readbuf

--- easyLA/tool/ConnectionSocket.cc
+++ easyLA/tool/ConnectionSocket.cc
@@ -1,9 +1,37 @@
 #include "ConnectionSocket.h"
+#include "Buffer.h"
+#include "SocketAPI.h"
 #include <unistd.h>
 
 using namespace LGG;
+using namespace std;
 
 size_t ConnectionSocket::DEFAULT_BUFSIZE = 1<<12;
+
+ConnectionSocket::ConnectionSocket(int fd, SocketAddr addr) 
+    : fd_(fd), addr_(make_unique<SocketAddr>(addr)), readBuf_(make_unique<Buffer>(DEFAULT_BUFSIZE)) 
+{
+    readBuf_->readMode();
+};
+
+ConnectionSocket::~ConnectionSocket() { SocketAPI::close(fd_); };
+
+size_t ConnectionSocket::readFd() {
+    readBuf_->writeMode();
+    auto res = readBuf_->putFromFd(fd_);
+    readBuf_->readMode();
+    return res;
+}
+
+std::string_view ConnectionSocket::readLine() {
+    std::string_view line;
+    while((line = readBuf_->getLine()).size() == 0){
+        readFd();
+    }
+    return line;
+}
+
+void ConnectionSocket::resizeBuf(size_t newSize) { readBuf_->resize(newSize); }
 
 void ConnectionSocket::write(std::string_view str){
     ::write(fd_, str.data(), str.size());
--- easyLA/tool/ConnectionSocket.h
+++ easyLA/tool/ConnectionSocket.h
@@ -1,46 +1,33 @@
 #pragma once
 
 #include "Noncopyable.h"
-#include "Buffer.h"
-#include "SocketAPI.h"
+#include <bits/unique_ptr.h>
+
 
 namespace LGG
 {
+class Buffer;
+class SocketAddr;
 
 class ConnectionSocket : Noncopyable {
-    //一直处于读模式
-    Buffer readBuf_;
+    std::unique_ptr<Buffer> readBuf_;
+    std::unique_ptr<SocketAddr> addr_;
     int fd_;
-    SocketAddr addr_;
     static size_t DEFAULT_BUFSIZE;
 public:
+    ConnectionSocket(int fd, SocketAddr addr);
 
-    ConnectionSocket(int fd, SocketAddr addr) : fd_(fd), addr_(addr), readBuf_(DEFAULT_BUFSIZE) {
-        readBuf_.readMode();
-    };
+    ~ConnectionSocket();
 
-    ~ConnectionSocket() { SocketAPI::close(fd_); };
+    size_t readFd();
 
-    size_t readFd() {
-        readBuf_.writeMode();
-        auto res = readBuf_.putFromFd(fd_);
-        readBuf_.readMode();
-        return res;
-    }
+    std::string_view readLine();
 
-    std::string_view readLine() {
-        std::string_view line;
-        while((line = readBuf_.getLine()).size() == 0){
-            readFd();
-        }
-        return line;
-    }
-
-    const SocketAddr& getAddr() const { return addr_; }
+    const SocketAddr& getAddr() const { return *addr_; }
 
     void write(std::string_view str);
 
-    void resizeBuf(size_t newSize) { readBuf_.resize(newSize); }
+    void resizeBuf(size_t newSize);
 
     static void setDEFAULT_BUFSIZE(size_t size) {
         DEFAULT_BUFSIZE = size;
